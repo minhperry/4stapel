a739659d22579e2aea09081940a37b4ff62c1134
Tuan Minh Phan <me@minhperry.de>
2025-08-07 21:38:26 +0200

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.minhperry.game.fourstapel',
+  appName: '4Stapel',
+  webDir: 'dist'
+};
+
+export default config;
