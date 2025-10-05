38bd4b1b1c9abd5d63c93c2897e0db556981c7eb
Tuan Minh Phan <me@minhperry.de>
2025-10-05 16:46:25 +0200

    main page

--- src/app/pages/main-menu/main-menu.page.ts
+++ src/app/pages/main-menu/main-menu.page.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {ButtonComponent} from '@components';
+import {CardManager} from '@services';
+import {CardComponent} from '@components';
+import {Card} from '../../engine/models';
 
 @Component({
   selector: 'app-main-menu',
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    CardComponent,
   ],
   templateUrl: './main-menu.page.html',
   styleUrl: './main-menu.page.scss'
 })
 export class MainMenuPage {
-
+  cardList: Card[] = []
+  constructor(private cm: CardManager) {
+    this.cardList = cm.cards
+  }
 }
