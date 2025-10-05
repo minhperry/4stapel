218ff4fe4d8c3e7a5e9da571873072285f5a7921
Tuan Minh Phan <me@minhperry.de>
2025-10-05 16:48:07 +0200

    add view constants

---
+++ src/app/utils/constants.ts
@@ -0,0 +1,16 @@
+export const AMOUNT = 98
+
+export const OKLCH_COLOR_CONSTANTS = {
+  LIGHTNESS: [0.88, 0.55],
+  CHROMA: [0.18, 0.20],
+  HUE: [90, 30],
+}
+
+export const OKLCH_COLOR_CONSTANTS2 = {
+  // Starts very light (90%) and ends much darker (50%)
+  LIGHTNESS: [0.9, 0.5],
+  // Starts with gentle saturation and gets richer as it darkens
+  CHROMA: [0.10, 0.16],
+  // Moves from a bright sky blue (220) to a deep indigo (280)
+  HUE: [220, 280],
+}
